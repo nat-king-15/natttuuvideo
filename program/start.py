cf94d1802cd191d731b4e92abe58cfa9dd068d80
nat-king-15 <66466499+nat-king-15@users.noreply.github.com>
2022-02-05 09:24:03 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -87,7 +87,7 @@ async def start_(client: Client, message: Message):
                 ],
                 [
                     InlineKeyboardButton(
-                        "🌐 WhatsAapc", url="https://wa.me/qr/XALM4UGBUJJWI1"
+                        "🌐 WhatsAap", url="https://wa.me/qr/XALM4UGBUJJWI1"
                     )
                 ],
             ]
